b139e6aca035f5be90e9c6ea60db938b99a1dcb7
Alison Pariela <parielaalison@gmail.com>
2026-06-28 21:19:07 +0400

    feat(frontend): Migrate project to typescript

---
+++ frontend/cyrus/src/declaration.d.ts
@@ -0,0 +1,21 @@
+
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.pmg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
